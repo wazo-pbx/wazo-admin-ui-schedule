aded18c4598c33b52caa7bd75211bd929a617cac
Cedric Abunar <cabunar@wazo.io>
2018-01-31 14:19:10 -0500

    init_time_picker on each new row_entry

--- wazo_plugind_admin_ui_schedule_official/static/js/schedule.js
+++ wazo_plugind_admin_ui_schedule_official/static/js/schedule.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+  init_time_picker()
+  $('.add-row-entry').click(function(e) {
+    init_time_picker()
+  });
+});
+
+function init_time_picker() {
   $('[class^=schedule_date_hours_]').timepicker({
     timeFormat: 'hh:mm',
   });
-});
+}
